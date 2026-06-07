3e573fbf1c61c8e003eaf3bd0e97bfc725209c83
Fusezion <fusezionstream@gmail.com>
2026-06-07 16:43:41 -0400

    IncomeSell.kt - update commands, configs and remove excellent economy

--- src/main/kotlin/dev/lyric/income/sell/IncomeSell.kt
+++ src/main/kotlin/dev/lyric/income/sell/IncomeSell.kt
@@ -1,43 +1,34 @@
 package dev.lyric.income.sell
 
-import dev.jorel.commandapi.CommandAPI
-import dev.jorel.commandapi.CommandAPIPaperConfig
-import dev.lyric.income.sell.api.IncomeSellAPI
+import dev.lyric.income.sell.commands.IncomeSellCommand
 import dev.lyric.income.sell.commands.SellCommand
 import dev.lyric.income.sell.commands.SellGuiCommand
 import dev.lyric.income.sell.commands.SellHandCommand
-import dev.lyric.income.sell.commands.SellwandCommand
 import dev.lyric.income.sell.config.ConfigManager
+import dev.lyric.income.sell.config.data.MessageConfig
+import dev.lyric.income.sell.config.data.SellGuiConfig
 import dev.lyric.income.sell.config.data.SellwandConfig
+import dev.lyric.income.sell.config.entry.FileConfigEntry
 import dev.lyric.income.sell.config.entry.FolderConfigEntry
 import dev.lyric.income.sell.listeners.SellGuiListener
 import dev.lyric.income.sell.listeners.SellwandUseListener
-import dev.lyric.income.sell.providers.ExcellentEconomyProvider
+import dev.lyric.income.sell.messages.Messages
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
 import org.bukkit.plugin.PluginManager
 import org.bukkit.plugin.java.JavaPlugin
-import su.nightexpress.excellenteconomy.api.ExcellentEconomyAPI
 
 class IncomeSell : JavaPlugin() {
 
 	companion object {
 		lateinit var instance: IncomeSell
-	}
-
-	override fun onLoad() {
-		CommandAPI.onLoad(CommandAPIPaperConfig(this).silentLogs(false).verboseOutput(true).setNamespace("incomesell"))
+		lateinit var messages: Messages
 	}
 
 	override fun onEnable() {
-		CommandAPI.onEnable()
 		instance = this
 		registerConfigs()
 		registerCommands()
-		registerDefaultProviders()
 		registerListeners(server.pluginManager)
-	}
-
-	override fun onDisable() {
-		CommandAPI.onDisable()
 	}
 
 	private fun registerListeners(pluginManager: PluginManager) {
@@ -46,21 +37,31 @@ class IncomeSell : JavaPlugin() {
 	}
 
 	private fun registerConfigs() {
-		val sellwandFolderConfig = FolderConfigEntry("sellwands", SellwandConfig.serializer(), listOf("common", "uncommon", "rare", "epic", "legendary", "mythic"))
+		val sellwandFolderConfig = FolderConfigEntry(
+			"sellwands",
+			SellwandConfig.serializer(),
+			listOf("common", "uncommon", "rare", "epic", "legendary", "mythic")
+		)
 		ConfigManager.registerFolder("sellwands", sellwandFolderConfig)
-		ConfigManager.loadAllConfigFiles()
+		val messagesFileConfig = FileConfigEntry(
+			"messages.yml",
+			MessageConfig.serializer(),
+			MessageConfig(),
+			onLoad = { messages = Messages(it) })
+		ConfigManager.registerFile("messages", messagesFileConfig)
+		ConfigManager.registerFile("sellgui", FileConfigEntry("sellgui.yml", SellGuiConfig.serializer()))
+		ConfigManager.loadAllConfigEntries()
+		messages = Messages(ConfigManager.getConfig("messages"))
 	}
 
 	private fun registerCommands() {
-		SellwandCommand.register()
-		SellCommand.register()
-		SellHandCommand.register()
-		SellGuiCommand.register()
-	}
-
-	private fun registerDefaultProviders() {
-		val economyProvider = server.servicesManager.getRegistration(ExcellentEconomyAPI::class.java) ?: return
-		IncomeSellAPI.registerProvider(ExcellentEconomyProvider(economyProvider.provider))
+		lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) {
+			val commands = it.registrar()
+			commands.register(SellCommand.createCommand())
+			commands.register(SellHandCommand.createCommand())
+			commands.register(SellGuiCommand.createCommand())
+			commands.register(IncomeSellCommand.createCommand())
+		}
 	}
 
 }
