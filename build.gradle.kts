b8052114b63834231c924c729eb84105b86d46f5
Fusezion <fusezionstream@gmail.com>
2026-06-13 03:00:37 -0400

    Updated build.gradle.kts to Kotlin4Bukkit and my config library

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,19 @@ plugins {
 	kotlin("jvm") version "2.4.0"
 	kotlin("plugin.serialization") version "2.4.0"
 	id("com.gradleup.shadow") version "9.4.1"
-	id("xyz.jpenilla.run-paper") version "3.0.2"
-	`maven-publish`
 }
 
 repositories {
 	mavenCentral()
 	maven("https://repo.papermc.io/repository/maven-public/")
-	maven("https://repo.nightexpressdev.com/releases")
+	maven("https://jitpack.io")
 }
 
 dependencies {
 	compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
-	implementation("net.mamoe.yamlkt:yamlkt:0.13.0")
+	compileOnly("net.mamoe.yamlkt:yamlkt:0.13.0")
+	compileOnly("com.github.Fusezion:Kotlin4Bukkit:1.0.0")
+	implementation("com.github.Fusezion:ConfigSerialization:1.1.2")
 }
 
 kotlin {
@@ -23,46 +23,24 @@ kotlin {
 
 java {
 	withSourcesJar()
+	withJavadocJar()
 }
 
 tasks {
 
 	shadowJar {
 		archiveClassifier = ""
+		relocate("dev.lyric.config", "dev.lyric.income.sell.libs.config")
 	}
 
 	build {
 		dependsOn(shadowJar)
 	}
 
-	runServer {
-		// Configure the Minecraft version for our task.
-		// This is the only required configuration besides applying the plugin.
-		// Your plugin's jar (or shadowJar if present) will be used automatically.
-//		downloadPlugins {
-//			modrinth("skript", "2.15.2")
-//			modrinth("excellenteconomy", "2.8.0")
-//			github("SkriptLang", "skript-reflect", "v2.6.3", "skript-reflect-2.6.3.jar")
-//			github("MilkBowl", "Vault", "1.7.3", "Vault.jar")
-//			modrinth("nightcore", "2.15.2")
-//		}
-		minecraftVersion("1.21.11")
-		jvmArgs("-Xms2G", "-Xmx2G", "-Dcom.mojang.eula.agree=true")
-	}
-
 	processResources {
-		val props = mapOf("version" to version, "description" to project.description)
+		val props = mapOf("version" to version)
 		filesMatching("paper-plugin.yml") {
 			expand(props)
-		}
-	}
-}
-
-publishing {
-	publications {
-		create<MavenPublication>("maven") {
-			artifact(tasks.shadowJar.get())
-			artifact(tasks.named("sourcesJar"))
 		}
 	}
 }
