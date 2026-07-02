9cb6a1ad7170940bdd19feb90dd64f8bc9636a53
Fusezion <fusezionstream@gmail.com>
2026-07-02 02:05:58 -0400

    Update IncomeSellAPI.kt to IncomeEconomy classes

--- src/main/kotlin/dev/lyric/income/sell/api/IncomeSellAPI.kt
+++ src/main/kotlin/dev/lyric/income/sell/api/IncomeSellAPI.kt
@@ -1,126 +1,187 @@
 package dev.lyric.income.sell.api
 
-import dev.lyric.income.economy.EconomyProviderRegistry
-import dev.lyric.income.sell.api.events.PlayerSellEvent
+import dev.lyric.income.economy.api.EconomyAPI
+import dev.lyric.income.economy.api.EconomyCollection
+import dev.lyric.income.economy.api.EconomyEntry
+import dev.lyric.income.economy.api.ProviderKey
+import dev.lyric.income.sell.api.event.PlayerSellEvent
+import dev.lyric.income.sell.api.event.PrePlayerSellEvent
+import dev.lyric.income.sell.api.multiplier.MultiplierProviderRegistry
+import dev.lyric.income.sell.utils.PDCKeys
 import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import org.bukkit.persistence.PersistentDataContainer
 import org.bukkit.persistence.PersistentDataType
 
 object IncomeSellAPI {
 
-	private val SELL_ACTION_KEY = NamespacedKey.fromString("incomesell:sell_actions")!!
-	private val PROVIDER_KEY_REGEX = Regex("[a-z0-9._-]+")
-
+	//region Sell Actions/Item Modifications
 	/**
-	 * Creates an empty unmodified sell result perfect for usage outside of selling items.
-	 */
-	@JvmStatic
-	fun createEmptySellResult(): SellResult = SellResult()
-
-	/**
-	 * Checks whether the provided [item] has the required information about sell actions attached.
-	 */
-	@JvmStatic
-	fun hasSellActions(item: ItemStack): Boolean {
-		return item.persistentDataContainer.has(SELL_ACTION_KEY, PersistentDataType.TAG_CONTAINER)
-				&& getSellActions(item).isNotEmpty()
-	}
-
-	/**
-	 * Adds a sell action to the persistent data container of the [item].
 	 *
-	 * @throws IllegalArgumentException if [item] is empty see [ItemStack.isEmpty]
-	 * @throws IllegalArgumentException if [amount] is less than or equal to 0
-	 * @throws IllegalArgumentException if [providerId] does not match the regex `[a-z0-9._-]`
-	 * @throws IllegalArgumentException if [argument] is not null and does not match the regex `[a-z0-9._-]`
+	 * ##### Important
+	 * This mutates the [item] passed through, if you don't want it mutated ensure you pass in a copy
+	 * and use the resulting item stack otherwise you're free to ignore the returned output
+	 *
+	 * @param[item] The item that will have the sell data attached
+	 * @param[collection] An economy collection consisting of the sell data to add
+	 *
+	 * @return The newly edited item stack, updated with the new sell data
+	 *
 	 */
 	@JvmStatic
-	fun addSellAction(item: ItemStack, providerId: String, amount: Double, argument: String? = null, overwrite: Boolean = false): ItemStack {
-		require(!item.isEmpty) { "Provided itemstack cannot be empty" }
-		require(amount > 0) { "Amount must not be less than or equal to 0" }
-		require(providerId.matches(PROVIDER_KEY_REGEX)) { "provider id does not match the regex [a-z0-9._-]" }
-		if (argument != null) require(argument.matches(PROVIDER_KEY_REGEX)) { "argument does not match the regex [a-z0-9._-]" }
-
-		val sellActions = getSellActions(item)
-		val providerId = providerId.lowercase()
-		val argument = argument?.lowercase()
-		val key = if (argument.isNullOrBlank()) providerId else "${providerId}/${argument}"
-		sellActions[key] = if (overwrite) amount else amount.plus(sellActions.getOrDefault(key, 0.0))
-		setSellActions(item, sellActions)
+	fun addSellData(item: ItemStack, collection: EconomyCollection): ItemStack {
+		item.editPersistentDataContainer { container ->
+			val sellActions = container.getOrDefault(
+				PDCKeys.sellActions,
+				PersistentDataType.TAG_CONTAINER,
+				container.adapterContext.newPersistentDataContainer()
+			)
+			editSellActions(sellActions, collection)
+			if (sellActions.isEmpty) return@editPersistentDataContainer
+			container.set(PDCKeys.sellActions, PersistentDataType.TAG_CONTAINER, sellActions)
+		}
 		return item
 	}
 
 	/**
-	 * Gets the sell-actions of the provided [item]
+	 *
+	 * ##### Important
+	 * This mutates the [item] passed through, if you don't want it mutated ensure you pass in a copy
+	 * and use the resulting item stack otherwise you're free to ignore the returned output
+	 *
+	 * @param[item] The item that will have the sell data attached
+	 * @param[entry] The entry consisting of the sell data to add
+	 *
+	 * @return The newly edited item stack, updated with the newly added sell data
+	 *
 	 */
 	@JvmStatic
-	fun getSellActions(item: ItemStack): MutableMap<String, Double> {
-		val sellActions = mutableMapOf<String, Double>()
-		val dataContainer = item.persistentDataContainer
-		val sellActionsContainer = dataContainer.get(SELL_ACTION_KEY, PersistentDataType.TAG_CONTAINER) ?: return sellActions
-		for (key in sellActionsContainer.keys) {
-			if (!sellActionsContainer.has(key, PersistentDataType.DOUBLE)) continue
-			val amount = sellActionsContainer.get(key, PersistentDataType.DOUBLE) ?: continue
-			sellActions[key.key] = amount
-		}
-		return sellActions
+	fun addSellData(item: ItemStack, entry: EconomyEntry): ItemStack {
+		val providerKey = entry.key
+		return addSellData(item, providerKey.provider, providerKey.argument, entry.amount)
 	}
 
 	/**
-	 * Overwrites the sell-actions of the provided [item] to everything inside the [sellActions] map
+	 *
+	 * ##### Important
+	 * This mutates the [item] passed through, if you don't want it mutated ensure you pass in a copy
+	 * and use the resulting item stack otherwise you're free to ignore the returned output
+	 *
+	 * @param[item] The item that will have the sell data attached
+	 * @param[providerKey]
+	 * @param[amount]
+	 *
+	 * @return The newly edited item stack, updated with the newly add sell data
+	 *
 	 */
 	@JvmStatic
-	fun setSellActions(item: ItemStack, sellActions: Map<String, Double>) {
-		item.editPersistentDataContainer { persistentDataContainer ->
-			val sellActionPDC = persistentDataContainer.adapterContext.newPersistentDataContainer()
-			for ((sellKey, amount) in sellActions) {
-				val sellActionKey = NamespacedKey.fromString("incomesell:$sellKey") ?: continue
-				sellActionPDC.set(sellActionKey, PersistentDataType.DOUBLE, amount)
+	fun addSellData(item: ItemStack, providerKey: ProviderKey, amount: Double): ItemStack {
+		return addSellData(item, providerKey.provider, providerKey.argument, amount)
+	}
+
+	@JvmStatic
+	fun addSellData(item: ItemStack, providerId: String, argument: String?, amount: Double): ItemStack {
+		item.editPersistentDataContainer { container ->
+			val sellActions = container.getOrDefault(
+				PDCKeys.sellActions,
+				PersistentDataType.TAG_CONTAINER,
+				container.adapterContext.newPersistentDataContainer()
+			)
+			val provider = EconomyAPI.getProvider(providerId) ?: return@editPersistentDataContainer
+			if (argument != null && !provider.validateArgument(argument)) return@editPersistentDataContainer
+			val namespacedKey = getNamespacedKey(providerId, argument) ?: return@editPersistentDataContainer
+			sellActions.set(namespacedKey, PersistentDataType.DOUBLE, amount)
+			container.set(PDCKeys.sellActions, PersistentDataType.TAG_CONTAINER, sellActions)
+		}
+		return item
+	}
+
+	@JvmStatic
+	fun setSellData(item: ItemStack, collection: EconomyCollection): ItemStack {
+		item.editPersistentDataContainer { container ->
+			val sellActions = container.adapterContext.newPersistentDataContainer()
+			editSellActions(sellActions, collection)
+			if (sellActions.isEmpty) return@editPersistentDataContainer
+			container.set(PDCKeys.sellActions, PersistentDataType.TAG_CONTAINER, sellActions)
+		}
+		return item
+	}
+
+	@JvmStatic
+	fun clearSellData(item: ItemStack): ItemStack {
+		item.editPersistentDataContainer { it.remove(PDCKeys.sellActions) }
+		return item
+	}
+
+	@JvmStatic
+	fun hasSellData(item: ItemStack): Boolean {
+		return !item.isEmpty && item.persistentDataContainer.has(PDCKeys.sellActions)
+	}
+
+	@JvmStatic
+	fun getSellData(item: ItemStack): EconomyCollection? {
+		if (!hasSellData(item)) return null
+		val container =
+			item.persistentDataContainer.get(PDCKeys.sellActions, PersistentDataType.TAG_CONTAINER) ?: return null
+		val entries = mutableListOf<EconomyEntry>()
+		val itemAmount = item.amount
+		for (key in container.keys) {
+			val providerKeyString = key.key.split("/", limit = 2)
+			val providerId = providerKeyString.first()
+			val argument = providerKeyString.getOrNull(1)
+			val provider = EconomyAPI.getProvider(providerId) ?: continue
+			if (argument != null && !provider.validateArgument(argument)) continue
+			val amount = container.get(key, PersistentDataType.DOUBLE) ?: continue
+			entries += EconomyEntry(ProviderKey(providerId, argument), (amount * itemAmount))
+		}
+		return EconomyCollection(entries)
+	}
+	//endregion
+
+	//region Sell Result
+	@JvmStatic
+	fun createSellReport(): SellReport = SellReport()
+
+	fun processSell(player: Player, sellReport: SellReport): Boolean {
+		val prePlayerSell = PrePlayerSellEvent(player, sellReport).callEvent()
+		if (!prePlayerSell) return false
+		MultiplierProviderRegistry.getProviders().forEach { multiplierProvider ->
+			if (multiplierProvider.canApply(player))
+				multiplierProvider.applyMultipliers(player, sellReport)
+		}
+		sellReport.getTransactions().map { entry ->
+			for (multiplier in sellReport.getMultipliers().filter { it.canApplyTo(entry) }) {
+				multiplier.applyTo(entry)
 			}
-			persistentDataContainer.set(SELL_ACTION_KEY, PersistentDataType.TAG_CONTAINER, sellActionPDC)
-		}
+			return@map entry
+		}.let { EconomyCollection(it).deposit(player) }
+		sellReport.markCompleted()
+		PlayerSellEvent(player, sellReport).callEvent()
+		return true
 	}
 
-	@JvmStatic
-	fun payoutTransactions(player: Player, sellResult: SellResult): Boolean {
-		val wasCancelled = !PlayerSellEvent(player, sellResult).callEvent()
-		if (wasCancelled) return false
-		val validTransactions = sellResult.getTransactions().filter { isValidProvider(it.key) }.toMutableMap()
-		val itemTransactions = sellResult.getItemTransactions()
-		for (itemTransaction in itemTransactions) {
-			for ((providerKey, amount) in itemTransaction.transactions) {
-				if (!isValidProvider(providerKey)) continue
-				validTransactions.merge(providerKey, amount, Double::plus)
-			}
-		}
-		if (validTransactions.isEmpty()) return false
-		var paidOutAnything = false
-		for ((providerKey, amount) in validTransactions) {
-			if (!isValidProvider(providerKey)) continue
-			val (providerId, argument) = getProviderAndArgument(providerKey)
-			val provider = EconomyProviderRegistry.get(providerId) ?: continue
-			val multiplier = sellResult.calculateMultiplier(providerKey)
-			val finalAmount = amount * multiplier
-			provider.deposit(player, finalAmount, argument)
-			if (!paidOutAnything) paidOutAnything = true
-		}
-		return paidOutAnything
+	//endregion
+
+	//region Utility Private Methods
+	private fun getNamespacedKey(providerId: String, argument: String?): NamespacedKey? {
+		val key = if (argument != null) "$providerId/$argument" else providerId
+		return NamespacedKey.fromString("incomesell:$key")
 	}
 
-	@JvmStatic
-	internal fun getProviderAndArgument(providerKey: String): Pair<String, String?> {
-		val parts = providerKey.split("/", limit = 2)
-		val provider = parts[0]
-		val argument = parts.getOrNull(1)
-		return Pair(provider, argument)
+	private fun editSellActions(
+		container: PersistentDataContainer,
+		collection: EconomyCollection
+	): PersistentDataContainer {
+		for ((providerKey, amount) in collection.getCompactedEntries()) {
+			val (providerId, argument) = providerKey
+			val provider = EconomyAPI.getProvider(providerId) ?: continue
+			if (argument != null && !provider.validateArgument(argument)) continue
+			val namespacedKey = getNamespacedKey(providerId, argument) ?: continue
+			container.set(namespacedKey, PersistentDataType.DOUBLE, amount)
+		}
+		return container
 	}
-
-	@JvmStatic
-	internal fun isValidProvider(providerKey: String): Boolean {
-		val (providerId, argument) = getProviderAndArgument(providerKey)
-		val provider = EconomyProviderRegistry.get(providerId) ?: return false
-		return argument == null || provider.validateArgument(argument)
-	}
+	//endregion
 
 }
